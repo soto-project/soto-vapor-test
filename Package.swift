c8c1b48aa6ee324f950324277318e4ba7d07c973
Adam Fowler <adamfowler71@gmail.com>
2020-10-28 15:59:16 +0000

    Use soto not aws-sdk-swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(name: "AWSSDKSwift", url: "https://github.com/swift-aws/aws-sdk-swift.git", from: "4.0.0")
+        .package(name: "AWSSDKSwift", url: "https://github.com/soto-project/soto.git", from: "4.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: [
